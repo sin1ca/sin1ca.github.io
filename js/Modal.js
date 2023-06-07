d65692b3c8e332d8848bf5ab478222dcd7ea5ec5
sin1ca <120512442+sin1ca@users.noreply.github.com>
2023-06-08 02:40:13 +0300

    Delete js directory

--- js/Modal.js
+++
@@ -1,6 +0,0 @@
-const cookies = document.getElementById("cookies");
-const cookiesBtn = document.getElementById("cookies__btn");
-
-cookiesBtn.addEventListener("click", function () {
-    cookies.style.display = "none";
-});
